20efef43b665f4c0140584c5e3e95b3639cc2b4d
panukettu <panu@0xp.lol>
2023-11-03 06:47:15 +0200

    feat: tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,70 +1,70 @@
-import { execSync } from 'child_process';
-import { existsSync, mkdirSync, unlinkSync } from 'fs';
+import { execSync } from "child_process"
+import { existsSync, mkdirSync, unlinkSync } from "fs"
 
 // @ts-expect-error
-import pkg from 'glob';
-import { defineConfig } from 'tsup';
-const { glob } = pkg;
+import pkg from "glob"
+import { defineConfig } from "tsup"
+const { glob } = pkg
 // const { glob } = pkg;
 
 export default defineConfig(async (opts) => {
 	return {
-		entry: ['src/extension.ts', 'src/server.ts'],
-		external: ['vscode', 'typescript'],
-		format: 'cjs',
-		outDir: './out/src',
-		platform: 'node',
+		entry: ["src/extension.ts", "src/server.ts"],
+		external: ["vscode", "typescript"],
+		format: "cjs",
+		outDir: "./out/src",
+		platform: "node",
 		sourcemap: false,
 		bundle: true,
 		noExternal: [
 			// /(?:[^vscode]).*?([\w\-]+)/,
-			'@iarna/toml',
-			'axios',
-			'fs-extra',
-			'glob',
-			'fast-equals',
-			'solc',
-			'solhint',
-			'lodash.debounce',
-			'prettier',
-			'prettier-plugin-solidity',
-			'solparse-exp-jb',
-			'vscode-languageclient',
-			'vscode-languageserver',
-			'vscode-languageserver-textdocument',
-			'vscode-uri',
-			'yaml-js',
-			'viem',
+			"@iarna/toml",
+			"axios",
+			"fs-extra",
+			"glob",
+			"fast-equals",
+			"solc",
+			"solhint",
+			"lodash.debounce",
+			"prettier",
+			"prettier-plugin-solidity",
+			"solparse-exp-jb",
+			"vscode-languageclient",
+			"vscode-languageserver",
+			"vscode-languageserver-textdocument",
+			"vscode-uri",
+			"yaml-js",
+			"viem",
 		],
 		treeshake: true,
 		splitting: true,
-		tsconfig: './tsconfig.json',
-		minify: false,
+		tsconfig: "./tsconfig.json",
+		minify: true,
 		clean: true,
 		async onSuccess() {
-			if (opts.watch) return;
-			execSync('cp ./package.json ./out/package.json');
-			execSync('cp ./node_modules/@solidity-parser/parser/dist/*.tokens ./out/src/');
-			if (!existsSync('./out/src/antlr')) {
-				mkdirSync('./out/src/antlr', {});
+			if (opts.watch) return
+			execSync("cp ./package.json ./out/package.json")
+			execSync("cp ./node_modules/@solidity-parser/parser/dist/*.tokens ./out/src/")
+			if (!existsSync("./out/src/antlr")) {
+				mkdirSync("./out/src/antlr", {})
 			}
-			execSync('cp ./node_modules/@solidity-parser/parser/dist/antlr/*.tokens ./out/src/antlr');
+			execSync("cp ./node_modules/@solidity-parser/parser/dist/antlr/*.tokens ./out/src/antlr")
 
 			const files = [
-				...glob.sync('./out/src/postcss-*.js'),
-				...glob.sync('./out/src/angular-*.js'),
-				...glob.sync('./out/src/flow-*.js'),
-				...glob.sync('./out/src/typescript-*.js'),
-				...glob.sync('./out/src/babel-*.js'),
-				...glob.sync('./out/src/yaml-*.js'),
-			];
+				...glob.sync("./out/src/postcss-*.js"),
+				...glob.sync("./out/src/angular-*.js"),
+				...glob.sync("./out/src/flow-*.js"),
+				...glob.sync("./out/src/typescript-*.js"),
+				...glob.sync("./out/src/babel-*.js"),
+				...glob.sync("./out/src/yaml-*.js"),
+			]
 
-			console.debug('deleted', files);
+			console.debug("deleted", files)
 			if (files.length > 0) {
 				for (const file of files) {
-					unlinkSync(file);
+					unlinkSync(file)
 				}
 			}
 		},
-	};
-});
+	}
+})
